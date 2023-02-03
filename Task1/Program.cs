ebd3ed4d2ee55a760a76b21d4eec44610a4eaab7
Maik <samara.maik@yandex.by>
2023-02-03 16:10:07 +0300

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+System.Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int b= Convert.ToInt32(Console.ReadLine());
+int expon = a;
+for (int i = 1; i < b; i++)
+{
+    expon = expon*a;
+}
+System.Console.WriteLine("expon =" + expon);
