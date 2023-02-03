ef60e7ab1c4aff1fa3a9b06adb408a36d3f4737b
Maik <samara.maik@yandex.by>
2023-02-03 17:33:32 +0300

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран.
+int [] array = new int [8];
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(1, 100);
+    System.Console.WriteLine(array[i]);
+}
